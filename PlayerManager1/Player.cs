013864a992166286d2a5e7cba73d30c56c7f9257
a22007432 <ssm.antonio@gmail.com>
2024-04-26 19:59:51 +0100

    ex5

---
+++ PlayerManager1/Player.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerManager1
+{
+    public class Player
+    {
+       public string Name{ get; }
+
+        public int Score { get; set; }
+
+        public Player(string name, int score)
+        {
+            Score = score;
+            Name = name; 
+        }
+    }
+}
